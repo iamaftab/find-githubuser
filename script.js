cf60f2beaaf87d21a6c7939d71420429b3825b63
Aftab Jamil <iamaftabjamil@gmail.com>
2020-12-05 13:40:41 +0530

    Github Api integration & data access

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-$(document).ready(() => {
-    $('#searchBtn').on('keyup', (e) => {
-       let userName = e.target.value;
+$(document).ready(function() {
+    $('#searchBtn').on('click', function() {
+       let userName = $('#searchKey').val();
 
        $.ajax({
            url:'http://api.github.com/users/' + userName,
            data: {
-               client_id:'',
-               client_secret:''
+               //add your github client details
            }
-       }).done((userData) => {
-           console.log(userData);
-       })
-    })
-})
+       }).done(function(userData) {
+           //console.log(userData);
+           $('#profile').html(`
+           <div class="shadow2">
+               <div class="avatar">
+                    <img src="${userData.avatar_url}" class="shadow" alt="avatar">
+                </div>
+                <div class="username">
+                    <h2 class="name">${userData.name}</h2>
+                    <p class="userId">@${userData.login}</p>
+                    <p>
+                        <h4>${userData.bio}</h4>
+                        <h4>${userData.location}</h4>
+                        <h4>Blog: <a href="https://${userData.blog}" target="_blank">${userData.blog}</a></h4>
+                    </p>
+                </div>
+                </div>
+           `);
+       });
+
+       $.ajax({
+        url:'http://api.github.com/users/' + userName +'/repos',
+        data: {
+            //add your github client details
+        }
+       }).done((repos) => {
+           //console.log(repos);
+           $('.repos').html("");
+           $.each(repos, (index, repo) =>{
+            $('.repos').append(`
+            <div class="repo_item shadow1">
+                 <div class="details">
+                     <h2 class="repo_title">${repo.name}</h2>
+                     <p class="repo_desc">${repo.description}</p>
+                 </div>
+                 <a href="${repo.html_url}" target="_blank" class="repo_link">Goto Repo</a>
+             </div>
+            `)
+           });
+       });
+    });
+});
